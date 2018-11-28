e2381b1ec5181da7ca50755898a820d5cefa2cbb
MichaelM3 <dagreekarican@aim.com>
2018-11-28 09:16:30 -0500

    finished and tested code

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,44 @@
 class Gym
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  #Gets a list of all lifters that have a membership to a specific gym
+  def all_lifters
+      self.memberships.map do |membership|
+        membership.lifter
+      end
+  end
+
+  #helper method (our joiner)(this gets us all memberships at a specific gym)
+  def memberships
+    Membership.all.select do |membership|
+      membership.gym == self
+    end
+  end
+
+  #Gets a list of the names of all the lifters that have a membership
+  #to that gym
+  def all_lifter_names
+    self.all_lifters.map do |elements|
+      elements.name
+    end
+  end
+
+  #Gets the combined lift total of every lifter at a specific gym
+  def average_lift_total
+    lifts = self.all_lifters.reduce(0) { |acc, inst| acc + inst.lift_total}
+    lifts.to_f / self.all_lifters.length
+  end
+
 end
--- lib/membership.rb
+++ lib/membership.rb
@@ -1,7 +1,20 @@
 class Membership
-  attr_reader :cost
+  attr_reader :cost, :gym, :lifter
 
-  def initialize(cost)
+  @@all = []
+
+  def initialize(cost, gym, lifter)
     @cost = cost
+    @lifter = lifter
+    @gym = gym
+
+      #Gets a list of all memberships
+    @@all << self
   end
+
+    #Class methoad for all memberships
+  def self.all
+    @@all
+  end
+
 end
